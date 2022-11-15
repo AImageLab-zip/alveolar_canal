f17cb85f86d1a0f36197f089154e6957e2b2f4ef
vittoriopipoli <vittoriopipoli@gmail.com>
2022-11-15 12:34:14 +0100

    Positional Encoding vs TorchPositiionalEncoding

--- models/TransPosPadUNet3D.py
+++ models/TransPosPadUNet3D.py
@@ -1,20 +1,42 @@
 import torch
 import torch.nn as nn
 from torch import dropout, nn, Tensor
+import math
+import numpy as np
+
+def get_angles(pos, i, d_model):
+  angle_rates = 1 / torch.pow(10000, (2 * (i//2)) / torch.tensor(d_model, dtype=torch.float32))
+  return pos * angle_rates  
 
 class PositionalEncoding(nn.Module):
-    def __init__(self, d_model: int, dropout: float = 0.1, max_len: int = 5000):
+    def __init__(self, d_model, dropout = 0.1, max_len= 5000):
+        super().__init__()
+        self.dropout = nn.Dropout(p=dropout)
+        pe = get_angles(torch.arange(max_len).unsqueeze(-1), torch.arange(d_model).unsqueeze(0), d_model)
+        pe[:, 0::2]  = torch.sin(pe[:, 0::2])
+        pe[:, 1::2]  = torch.cos(pe[:, 0::2])
+        self.register_buffer('pe', pe)
+        print(pe.shape)
+    def forward(self, x):
+        x = x + self.pe[:x.size(1)]
+        return self.dropout(x)     
+
+class PositionalEncodingTorchDoc(nn.Module):
+    def __init__(self, d_model, dropout = 0.1, max_len= 5000):
         super().__init__()
         self.dropout = nn.Dropout(p=dropout)
         position = torch.arange(max_len).unsqueeze(1)
-        div_term = torch.exp(torch.arange(0, d_model, 2) * (-torch.log(10000.0) / d_model))
+        div_term = torch.exp(torch.arange(0, d_model, 2) * (-math.log(10000.0) / d_model))
         pe = torch.zeros(max_len, 1, d_model)
         pe[:, 0, 0::2] = torch.sin(position * div_term)
         pe[:, 0, 1::2] = torch.cos(position * div_term)
         self.register_buffer('pe', pe)
-    def forward(self, x: Tensor) -> Tensor:
+        print(pe.shape)
+    def forward(self, x):
         x = x + self.pe[:x.size(0)]
         return self.dropout(x)  
+
+      
 
 class TransformerBlock(nn.Module):
     def __init__(self, embed_dim, num_heads, ff_dim, seq_len, rate=0.1, batch_first=True,):
